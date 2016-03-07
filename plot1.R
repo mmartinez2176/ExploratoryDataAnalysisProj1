601e12e397222d8392fbe1161e942e17caa88d93
Martin <mmartinez2176@users.noreply.github.com>
2016-03-06 23:12:23 -0800

    Added files via upload

---
+++ plot1.R
@@ -0,0 +1,36 @@
+# Exploratory Data Analysis
+# Author: Martin Martinez
+# 
+
+#The purpose of this R-Script is to reproduce the the Plot 1 graph
+
+#Step 1:  Load the Libraries if necessary
+library(data.table)
+library(dplyr)
+library(png)
+
+#Step 2.  Load the data files
+
+# set the working dir 
+#myWorkingDir <- "/Volumes/User Data 3TB/Users/mmartine/Documents/Education/Coursera/Data Science Certificate/04. Exploratory Data Analysis"
+myWorkingDir <- "/home/mmartine/Documents/Education/Corusera/Exploratory-Data-Analysis/Proj1"
+setwd(myWorkingDir)
+
+#create the Data directory if needed
+
+if (!file.exists("./data")){
+  dir.create("./data")
+}
+
+#At this point we would normally pull the raw file from a different location eg URL location
+# but since the data was provided we'll, I've unzipped the file in the data direction within the working directory
+# So all that is needed to be done is load the data
+
+myData <- tbl_df(read.table("./data/household_power_consumption.txt",header=TRUE, sep=";", dec=".", stringsAsFactors=FALSE))
+myDataSubset <- myData %>% filter(Date==c("1/2/2007","2/2/2007"))
+gap <- as.numeric(myDataSubset$Global_active_power)
+
+#create png file
+png("plot1.png", width=480, height=480)
+hist(gap, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab = "frequency")
+dev.off()
